659713ddfe820f02617160937f73dfa0b560ff00
Ruta Reisoglu <rutareiso@protonmail.com>
2022-01-18 16:59:04 -0500

    Displau DayListItems in a DayList component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,6 +3,17 @@ import classNames from "classnames";
 import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
+  const formatSpots = (spots) => {
+    switch (spots) {
+      case 0:
+        return "no spots remaining";
+      case 1:
+        return "1 spot remaining";
+      default:
+        return `${spots} spots remaining`;
+    }
+  };
+
   let dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots === 0,
@@ -11,7 +22,7 @@ export default function DayListItem(props) {
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>;
     </li>
   );
 }
